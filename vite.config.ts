5811717eff3e0b6886fbdc55648b1ed971733f26
dejayillegal <dejayillegal@gmail.com>
2025-07-09 23:07:05 +0530

    Fix: the correct index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,17 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default ({ mode }) => {
+  // load all env vars (including VITE_BASE_PATH)
   const env = loadEnv(mode, process.cwd(), '');
   const base = env.VITE_BASE_PATH || '/';
 
   return defineConfig({
     root: 'client',
-    base,
+    base,  // e.g. '/thecueroom/' in production
     plugins: [react()],
     resolve: {
-      alias: { 
-        '@': path.resolve(__dirname, 'client/src') 
+      alias: {
+        '@': path.resolve(__dirname, 'client/src'),
       },
     },
     build: {
